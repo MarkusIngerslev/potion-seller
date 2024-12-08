32bec5c2ec2b2b5f48fa77ffecdcaa095176dfe2
MarkusIngerslev <markusolsen2004@gmail.com>
2024-12-08 18:24:23 +0100

    Sat opgaven op med makeup tekst og styling

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// imports
+import * as Tree from "./datastruktur/tree.js";
+
+// Global variables
+
+window.addEventListener("load", init);
+
+function init() {
+  console.log("Javascript loaded");
+
+  
+}
